7e5a78cbe3557e7f006e9bc9238fc2c718077e1f
Matthew Chase <antishow@gmail.com>
2024-03-29 06:53:11 -0400

    Oh right need to build it lol

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    modules: [
+      path.resolve(`${__dirname}/src`),
+      'node_modules'
+    ],
+  },
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          }
+        }
+      }
+    ],
+  },
+};
